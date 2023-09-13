4dbd1c75683f323066034f45bced894c8af53190
Bluh7 <gabrielperazzo123456789@gmail.com>
2023-09-13 00:11:02 -0300

    specified pg module on database connection config

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize"
 import config from "./dotenv.js"
+import pg from "pg"
 
 const { PROD_DB_URL, DEV_DB_URL, NODE_ENV } = config
 
@@ -7,6 +8,8 @@ const sequelize = new Sequelize(
   NODE_ENV === "production" ? PROD_DB_URL : DEV_DB_URL,
   {
     logging: false,
+    dialectModule: pg,
+    dialect: "postgres",
     dialectOptions: {
       ssl: NODE_ENV === "production",
     },
