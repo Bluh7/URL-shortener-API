6eecad79bddcd381759b5959c91c309745589101
Bluh7 <gabrielperazzo123456789@gmail.com>
2023-09-13 00:45:46 -0300

    trying /temp folder for logs in prod

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,27 +1,7 @@
 import winston from "winston"
 import config from "../config/dotenv.js"
-import fs from "fs"
-import * as url from "url"
 
 const { NODE_ENV } = config
-
-const __dirname = url.fileURLToPath(new URL("../..", import.meta.url))
-
-// create folder and files if they don't exist in production
-if (NODE_ENV === "production") {
-  const logsDir = `${__dirname}logs`
-  if (!fs.existsSync(logsDir)) {
-    fs.mkdirSync(logsDir)
-  }
-  const errorLog = `${__dirname}logs/error.log`
-  if (!fs.existsSync(errorLog)) {
-    fs.writeFileSync(errorLog, "")
-  }
-  const combinedLog = `${__dirname}logs/combined.log`
-  if (!fs.existsSync(combinedLog)) {
-    fs.writeFileSync(combinedLog, "")
-  }
-}
 
 const logger = winston.createLogger({
   level: "info",
@@ -34,10 +14,16 @@ const logger = winston.createLogger({
     // Error level logs or less are written to error.log
     // Info level logs or less are written to combined.log
     new winston.transports.File({
-      filename: `${__dirname}logs/error.log`,
+      filename:
+        NODE_ENV === "production" ? "temp/logs/error.log" : "logs/error.log",
       level: "error",
     }),
-    new winston.transports.File({ filename: `${__dirname}logs/combined.log` }),
+    new winston.transports.File({
+      filename:
+        NODE_ENV === "production"
+          ? "temp/logs/combined.log"
+          : "logs/combined.log",
+    }),
   ],
 })
 
