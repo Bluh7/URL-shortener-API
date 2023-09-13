702a8d2149dc6b5e16e89faa55eae6359465da68
Bluh7 <gabrielperazzo123456789@gmail.com>
2023-09-13 01:17:11 -0300

    fixing render issue

--- src/app.js
+++ src/app.js
@@ -35,13 +35,14 @@ const corsOptions = {
 
 // __dirname is not available by default when using ES modules
 const __dirname = url.fileURLToPath(new URL("..", import.meta.url))
+const path = await import("path")
 
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(`${__dirname}public`))
 app.set("view engine", "ejs")
-app.set("views", "./src/views")
+app.set("views", path.join(`${__dirname}src`, "views"))
 app.use(router)
 
 export default app
